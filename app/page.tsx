3b38c5a51c834f703aaa4f2bff20c49842f7d3e4
Eugene Heynike <eugeneheynike@nexablend.co.za>
2024-07-02 15:16:40 +0200

    page is using the right component now

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import PdfViewer from './components/Viewer'
 export default function Home() {
   return (
-    <PdfViewer url={"/sample.pdf"}/>
+    <main className='py-'>
+      <PdfViewer url={'/sample.pdf'}/>
+    </main>
   )
 }
