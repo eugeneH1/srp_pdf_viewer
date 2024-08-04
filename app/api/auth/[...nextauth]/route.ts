7f9e3497360c86d38ad367abe06508064eb7449a
Eugene Heynike <eugeneheynike@nexablend.co.za>
2024-08-04 19:07:08 +0200

    switched to yarn. hold thumbs. this one's for you God

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { sql } from '@vercel/postgres';
 import { JWT } from 'next-auth/jwt';
 import { Session } from 'next-auth';
--- app/api/create_customer/route.ts
+++ app/api/create_customer/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { hash } from "bcrypt";
+import { hash } from "bcryptjs";
 import { sql } from '@vercel/postgres';
 import { randomBytes, createHmac } from "crypto";
 import nodemailer from "nodemailer";
